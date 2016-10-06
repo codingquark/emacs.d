f7cf58f729206e1db06e45b8f6d35d3cf2c65772
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2016-10-06 19:54:14 +0530

    More RSS feeds

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -9,6 +9,7 @@
         "http://codingquark.com/feed.xml"
         "https://medium.com/feed/@abrahamv23"
         "http://blog.stephenwolfram.com/feed/"
-        "https://www.lightbluetouchpaper.org/feed/"))
+        "https://www.lightbluetouchpaper.org/feed/"
+        "https://tanninpower.wordpress.com/feed/"))
 
 (provide 'init-elfeed)
