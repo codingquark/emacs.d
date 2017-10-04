bc54e2732ce6cdb427131364f8545fb0908e0a9f
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2017-10-04 16:39:06 +0530

    [FIX] Updated org config for breaking upstream changes

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -9,13 +9,27 @@
 (define-key global-map "\C-cc" 'org-capture)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline (concat org-directory "/todo/tasks.org") "Tasks")
+      '(("t" "Todo" entry (file+headline (lambda () (concat org-directory "/todo/tasks.org")) "Tasks")
          "* TODO %?\n %i\n %a")
-        ("s" "Shopping" entry (file+headline (concat org-directory "/todo/shopping.org") "List")
+        ("s" "Shopping" entry (file+headline (lambda () (concat org-directory "/todo/shopping.org")) "List")
          "* TODO %?\n %i")
-        ("p" "Paper" entry (file+headline (concat org-directory "/research/papers.org") "Links")
+        ("p" "Paper" entry (file+headline (lambda () (concat org-directory "/research/papers.org")) "Links")
          "* TODO %A")))
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; remove comments from org document for use with export hook
+;; https://emacs.stackexchange.com/questions/22574/orgmode-export-how-to-prevent-a-new-line-for-comment-lines
+(defun delete-org-comments (backend)
+  ;;; remove comments from org document for use with export hook
+  (loop for comment in (reverse (org-element-map (org-element-parse-buffer)
+                    'comment 'identity))
+    do
+    (setf (buffer-substring (org-element-property :begin comment)
+                (org-element-property :end comment))
+          "")))
+
+;; add to export hook
+(add-hook 'org-export-before-processing-hook 'delete-org-comments)
 
 (provide 'init-org)
 ;;; init-org ends here
