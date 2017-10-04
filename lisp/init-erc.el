2dfa32bdd8d05a20148b4bd5dd8fe230b8814a60
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2017-10-04 16:39:35 +0530

    [CLEANUP] Code was not related to ERC

--- lisp/init-erc.el
+++ lisp/init-erc.el
@@ -1,14 +1,7 @@
 ;; erc config
 (require 'secrets)
-(erc-autojoin-mode t)
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 (setq erc-paranoid t)
-
-;; better window switching
-(require 'switch-window)
-(setq-default switch-window-shortcut-style 'alphabet)
-(setq-default switch-window-timeout nil)
-(global-set-key (kbd "C-x o") 'switch-window)
 
 ;; ERC hook.
 ;; connects to freenode, identifies, joins channels,
