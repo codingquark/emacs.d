65c033c4dedb975192e0e341cbb264b991675ebe
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2017-11-08 17:07:24 +0530

    DDG as default w3m search engine

--- lisp/init-browser.el
+++ lisp/init-browser.el
@@ -8,6 +8,8 @@
 
 (setq browse-url-browser-function '(("\\(wikipedia.org\\)\\|\\(sprunge.us\\)" . w3m-browse-url)
                                     (".*" . browse-url-firefox)))
+;; Set ddg as default search engine
+(setq w3m-search-default-engine "duckduckgo")
 
 (provide 'init-browser)
 ;;; init-browser.el ends here
