a66df576f99647935340fba4a93ca7483ecd0094
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2018-05-11 11:28:37 +0530

    HTTPS only for melpa
    On #emacs parsnip said why do you have it? I realised he's right and I
    need to remove it. I use Linux systems only, so I have no use for win
    specific code here. Please note that I'm not removing it because it is
    "bad practise". I don't care. That code is useless for me, so it goes away.

--- init.el
+++ init.el
@@ -4,16 +4,10 @@
 ;;; Code:
 
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ;; Org-mode's repository
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ;; Org-mode's repository
+
 (package-initialize)
 
 ;; (require 'package)
