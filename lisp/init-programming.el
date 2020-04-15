c93e7762424a73124315e33dce7ca4198c0d0ddf
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2020-04-15 20:27:27 +0530

    Set Symbola if unicode

--- lisp/init-programming.el
+++ lisp/init-programming.el
@@ -7,19 +7,16 @@
 (global-auto-revert-mode 1)
 
 ;; (setq-default line-spacing 5) ;; Checkout `line-height` instead
+(set-face-attribute 'default t :font "Iosevka Nerd Font-13")
+;; The following will change the all existing frame's fonts
+;; (set-frame-font "Iosevka Nerd Font-13" t
+;;                 (frame-list))
 
-;; (set-face-attribute 'default t :font "Source Code Pro-11")
-(set-face-attribute 'default t :font "iosevka curly extended-11")
-;; The following will change the current frame's fonts
-;; (set-frame-font "Iosevka Curly Extended-11" nil t)
+(when (member "Symbola" (font-family-list))
+  (set-fontset-font t 'unicode "Symbola" nil 'prepend))
 
 (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 (add-hook 'dired-mode-hook 'auto-revert-mode)
-
-;; (add-to-list 'default-frame-alist '(font . "Source Code Pro-11" ))
-(add-to-list 'default-frame-alist '(font . "iosevka curly extended-11" ))
-
-;; (set-fontset-font t nil (font-spec :size 11 :name "Noto"))
 
 (defun extract-titles-from-arxiv ()
   "Extract titles of papers from arxiv subscription emails.
