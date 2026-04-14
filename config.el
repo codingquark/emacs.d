748b9433a1d491c6b08384f38118df2319e5ce17
Dhavan V <456712+codingquark@users.noreply.github.com>
2026-04-14 10:02:53 +0530

    Add Elfeed RSS reader setup

--- config.el
+++ config.el
@@ -23,13 +23,14 @@
   (setq initial-scratch-message "")
   
   ;; Start in denote directory
-  (setq initial-buffer-choice (lambda () (dired "~/Documents/notes"))))
+  ;; (setq initial-buffer-choice (lambda () (dired "~/Documents/notes")))
+  )
 
 (use-package modus-themes
   :init
   (load-theme 'modus-operandi t))
 
-(when (string= system-name "muon.local")
+(when (or (string= system-name "muon.local") (string= system-name "photon"))
   (set-face-attribute 'default nil :font "IBM Plex Mono" :height 160))
 
 (use-package emacs
@@ -45,14 +46,110 @@
   ;; Visual aids
   (global-display-line-numbers-mode 1)
   (show-paren-mode 1)
-  (global-auto-revert-mode 1)
-  
   ;; macOS compatibility
   (setq dired-use-ls-dired nil)
-  
+
+  :hook ((dired-mode . auto-revert-mode)
+         (prog-mode . auto-revert-mode)
+         (text-mode . auto-revert-mode))
+
   ;; Better keybindings
   :bind
   ("C-x C-b" . ibuffer))
+
+(use-package vertico
+  :init
+  (vertico-mode 1)
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package marginalia
+  :init
+  (marginalia-mode 1))
+
+(use-package consult
+  :bind (("C-s" . consult-line)
+         ("C-x b" . consult-buffer)
+         ("C-x 4 b" . consult-buffer-other-window)
+         ("M-y" . consult-yank-pop)
+         ("M-g g" . consult-goto-line)
+         ("M-g i" . consult-imenu)
+         ("M-s g" . consult-grep)
+         ("M-s r" . consult-ripgrep)))
+
+(use-package embark
+  :bind (("C-." . embark-act)
+         ("C-;" . embark-dwim)
+         ("C-h B" . embark-bindings))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command))
+
+(use-package embark-consult
+  :after (embark consult)
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package which-key
+  :init
+  (which-key-mode 1)
+  :custom
+  (which-key-idle-delay 0.5))
+
+(use-package helpful
+  :bind (([remap describe-command] . helpful-command)
+         ([remap describe-function] . helpful-callable)
+         ([remap describe-key] . helpful-key)
+         ([remap describe-symbol] . helpful-symbol)
+         ([remap describe-variable] . helpful-variable)))
+
+(use-package savehist
+  :ensure nil
+  :init
+  (savehist-mode 1))
+
+(use-package recentf
+  :ensure nil
+  :init
+  (recentf-mode 1)
+  :custom
+  (recentf-max-saved-items 200))
+
+(use-package saveplace
+  :ensure nil
+  :init
+  (save-place-mode 1))
+
+(use-package winner
+  :ensure nil
+  :init
+  (winner-mode 1))
+
+(defvar cq-elfeed-feeds-file
+  (expand-file-name "elfeed-feeds.el" user-emacs-directory)
+  "Path to the personal Elfeed feed list.")
+
+(use-package elfeed
+  :bind (("C-c f" . elfeed)
+         ("C-c F" . cq-open-elfeed-feeds-file))
+  :custom
+  (elfeed-db-directory (expand-file-name "elfeed" user-emacs-directory))
+  (elfeed-search-filter "@6-months-ago +unread")
+  :config
+  ;; Keep subscriptions outside the main config to make feed edits low-friction.
+  (load cq-elfeed-feeds-file 'noerror 'nomessage)
+
+  (defun cq-open-elfeed-feeds-file ()
+    "Open the Elfeed subscriptions file."
+    (interactive)
+    (find-file cq-elfeed-feeds-file)))
 
 (use-package dired
   :ensure nil
@@ -60,7 +157,7 @@
          (dired-mode . dired-hide-details-mode)))
 
 (use-package denote
-  :hook (find-file . denote-fontify-links-mode)
+  :hook (text-mode . denote-fontify-links-mode)
   :bind (
     ("C-c n n" . denote)
     ("C-c n D" . cq-open-denote-directory)
@@ -101,13 +198,11 @@
   :after denote)
 
 (use-package olivetti
-  :hook ((text-mode . olivetti-mode)
-         (org-mode . olivetti-mode))
+  :hook ((org-mode . olivetti-mode))
   :config
   (setq olivetti-body-width 80))
 
 (use-package markdown-mode
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+         ("\\.markdown\\'" . markdown-mode)))
---
+++ elfeed-feeds.el
@@ -0,0 +1,10 @@
+;;; elfeed-feeds.el --- Personal Elfeed subscriptions -*- lexical-binding: t; -*-
+
+(setq elfeed-feeds
+      '(("https://planet.emacslife.com/atom.xml" emacs)
+        ("https://sachachua.com/blog/category/emacs-news/feed/" emacs)
+        ("https://protesilaos.com/codelog.xml" emacs)
+        ("https://geohot.github.io/blog/feed.xml" programming)
+        ("http://blog.stephenwolfram.com/feed/" science)
+        ("https://vitalik.eth.limo/feed.xml" crypto)
+        ("https://stephango.com/feed.xml" blogs)))
